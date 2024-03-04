9f158c10e042a0d7ab4a1bf82157f69095056f3f
Jiaaaaooo <162094328+Jiaaaaooo@users.noreply.github.com>
2024-03-04 14:43:43 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from map.MapData import Map
+from particle.Particle import PSO
+import numpy as np
+
+
+# a = np.ones(5)
+# print(a)
+# r = a[0]
+# print(r)
+#
+# startSub = [[295, 110], [295, 154], [295, 169], [295, 216], [295, 224], [295, 230]]
+# snum = len(startSub)
+# print(snum)
+
+
+# x = np.array([[1,2],
+#               [2,3],
+#               [3,5]])
+# x_N = len(x)
+# print(x_N)
+# r=[]
+# for i in range(x_N):
+#     for j in range(i+1,x_N):
+#         r.append(np.sqrt(np.sum(np.square(x[i] - x[j]))))
+#
+# print(r)
+#
+# b = np.ones(5)*np.inf
+# print(b)
+H = np.ones(6)
+print(H)
+print(H[2])
